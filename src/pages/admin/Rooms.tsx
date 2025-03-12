d273409c9f24a5f054fcf5ff60afdea7f32629e5
sivajithms <sivajithm@gmail.com>
2025-03-12 19:51:59 +0530

    added admin navbar

---
+++ src/pages/admin/Rooms.tsx
@@ -0,0 +1,13 @@
+import AdminLayout from "@/layouts/AdminLayout"
+
+const Rooms = () => {
+    return (
+        <>
+        <AdminLayout>
+            hii
+        </AdminLayout>
+        </>
+    )
+}
+
+export default Rooms
