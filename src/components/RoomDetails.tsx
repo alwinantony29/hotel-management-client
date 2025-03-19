031a7981fa4f735be00a6d267bd1378892f988e1
Alwin <alwin@helloastral.com>
2025-03-19 13:36:10 +0530

    updates README file

--- src/components/RoomDetails.tsx
+++ src/components/RoomDetails.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Users, Maximize2, Wifi } from "lucide-react";
-import { useSearchParams } from "react-router";
+import { useNavigate, useSearchParams } from "react-router";
+import { useUser } from "@/store/useUser";
 
 const rooms = [
   {
@@ -38,6 +39,8 @@ const rooms = [
 
 export const RoomDetails: React.FC = () => {
   const [, setSearchParams] = useSearchParams();
+  const { user } = useUser();
+  const navigate = useNavigate();
 
   return (
     <section className="py-20 px-4 bg-gray-50" id="services">
@@ -85,15 +88,19 @@ export const RoomDetails: React.FC = () => {
 
                 <button
                   onClick={() => {
-                    setSearchParams((prev) => {
-                      prev.set("type", room.type);
-                      return prev;
-                    });
-                    document.getElementById("book")?.scrollIntoView();
+                    if (user) {
+                      setSearchParams((prev) => {
+                        prev.set("type", room.type);
+                        return prev;
+                      });
+                      document.getElementById("book")?.scrollIntoView();
+                    } else {
+                      navigate("/login");
+                    }
                   }}
                   className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition-colors"
                 >
-                  Book Now
+                  {user ? "Book Now" : "Get started"}
                 </button>
               </div>
             </div>
